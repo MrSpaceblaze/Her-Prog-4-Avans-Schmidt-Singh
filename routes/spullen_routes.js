d5fcf58b92396d3957db61fe6fdf7cda9a099195
MrSpaceblaze <32390661+MrSpaceblaze@users.noreply.github.com>
2018-07-02 13:36:06 +0200

    Revert "added routes"
    This reverts commit 9ae394ccd22b277ee02b1d0b02fc6cff57fd5751.

--- routes/spullen_routes.js
+++
@@ -1,9 +0,0 @@
-let routes = require('express').Router()
-let controller = require('../controllers/spullen_controller')
-
-routes.get('',controller.getAll)
-routes.post('',controller.postNew)
-
-routes.get('/:spullenID',controller.getBySpullenID)
-routes.put('/:spullenID',controller.putBySpullenID)
-routes.delete('/:spullenID',controller.deleteBySpullenID)
