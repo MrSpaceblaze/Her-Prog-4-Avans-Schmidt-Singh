1d6886ba79e94fc77a79dddad6a7ce237e1a47bb
MrSpaceblaze <32390661+MrSpaceblaze@users.noreply.github.com>
2018-07-02 13:29:07 +0200

    added routes

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // Require
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser');
-const auth_controller = require('./controller/auth_controller');
+const bodyParser = require('body-parser')
+const auth_controller = require('./controller/auth_controller')
+const categorie = require('./routes/categorie_routes');
 
 app.use(bodyParser.urlencoded({
     'extended': 'true'
@@ -12,15 +13,16 @@ app.use(bodyParser.json({
     type: 'application/vnd.api+json'
 }));
 
-app.use('/api/login',auth_controller.login)
-app.use('/api/register',auth_controller.register)
+app.post('/api/login',auth_controller.login)
+app.post('/api/register',auth_controller.register)
 app.all('*', auth_controller.validateToken);
-
+app.use('/api/categorie',)
 app.all("*", (req, res) => {
     res.status(404).json({
         error: "Endpoint not found"
     }).end()
 })
+
 // Listen
 let server = app.listen(8082, function () {
     let host = server.address().address
