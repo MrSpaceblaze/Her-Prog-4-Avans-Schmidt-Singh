e8758c3c783ac965e10014c55d840e2f958ea1cf
Akashdeep Singh <asingh4@avans.nl>
2018-07-03 11:04:18 +0200

    UserRegisterJSON Model

---
+++ models/UserRegisterJSON.js
@@ -0,0 +1,25 @@
+class UserRegisterJSON {
+    constructor(firstname, lastname, email, password) {
+        this.firstname = firstname
+        this.lastname = lastname
+        this.email = email
+        this.password = password
+    }
+
+    getFirstname() {
+        return this.firstname
+    }
+
+    getLastname() {
+        return this.lastname
+    }
+    
+    getEmail() {
+        return this.email
+    }
+
+    getPassword() {
+        return this.password
+    }
+}
+module.exports=UserLoginJSON
